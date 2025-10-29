5c9a93554cf31990c9042c4d2c84918adc089bbb
Camilin <cami.car.val@outlook.com>
2025-10-29 17:30:16 -0500

    docs: add basc documentation to service

--- app/main.py
+++ app/main.py
@@ -2,4 +2,6 @@ from fastapi import FastAPI
 from app.routers import items
 
 app = FastAPI()
+
+# Import the Item Roter
 app.include_router(items.router)
--- app/routers/items.py
+++ app/routers/items.py
@@ -5,19 +5,25 @@ router = APIRouter()
 
 items = []
 
+# Query All Items
 @router.get("/items/")
 async def read_items():
     return items
 
+# Create a single Item
 @router.post("/items/")
 async def create_item(item: dict):
     item["id"] = len(items) + 1
     items.append(item)
     return item
 
+# Query a single Item by its Id
 @router.get("/items/{item_id}")
 async def read_item(item_id: int):
+    # Search through all Items
     for item in items:
+        # If the Item is found, return it
         if item["id"] == item_id:
             return item
+    # If the Item is not found, return an error message
     return {"error": "Item not found"}
